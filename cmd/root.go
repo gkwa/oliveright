d16fc78e4c78fdfa271073ae1376baafdbb5af06
Taylor Monacelli <taylormonacelli@gmail.com>
2023-11-27 08:09:10 -0800

    Remove boilerplate

--- cmd/root.go
+++ cmd/root.go
@@ -55,10 +55,6 @@ func init() {
 
 	rootCmd.Flags().StringVar(&logFormat, "log-format", "", "json or text (default is text)")
 	viper.BindPFlag("log-format", rootCmd.Flags().Lookup("log-format"))
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
 // initConfig reads in config file and ENV variables if set.
